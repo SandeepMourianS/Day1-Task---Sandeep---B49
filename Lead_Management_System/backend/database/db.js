0aef1386e9a0467f6bdf6ddca3516d4fbfc23e68
Sandeep Mourian S <sandeepmourian10799@gmail.com>
2024-03-02 22:29:47 +0530

    Changed

--- Lead_Management_System/backend/database/db.js
+++ Lead_Management_System/backend/database/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 module.exports = () => {
 	const connectionParams = {
 		useNewUrlParser: true,
-		// useUnifiedTopology: true,
+		useUnifiedTopology: true,
 	};
 	try {
 		mongoose.connect("mongodb+srv://sandeepmourian10799:sandeep@cluster0.iw0ekps.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", connectionParams);
